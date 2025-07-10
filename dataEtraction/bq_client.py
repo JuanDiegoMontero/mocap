2c506ebda6a76babdf367437fd73f6d57065bbed
Holman Alv <alvholman@gmail.com>
2025-07-10 03:44:50 -0500

    update auth token

--- dataEtraction/bq_client.py
+++ dataEtraction/bq_client.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 # Ruta absoluta al JSON de credenciales
 BASE_DIR = Path(__file__).parent
-CRED_PATH = "./auth/tendencias-464719-9591593eb479.json"
+CRED_PATH = "./.auth/tendencias-464719-5864f0e22166.json"
 
 # Cargar credenciales explícitamente
 credentials = service_account.Credentials.from_service_account_file(
